3683e0d0c083e8447c1ebc56f2a8cc671603c0e1
Rian Tri Kusuma <riantk04@gmail.com>
2020-02-07 17:12:12 +0700

    Add files via upload

---
+++ theme/js/menu.js
@@ -0,0 +1,32 @@
+var menu = [
+	[
+		'home_menu',
+		'fa fa-home',
+		'Home'
+	],
+	[
+		'about_me',
+		'fa fa-user',
+		'About Me'
+	],
+	[
+		'my_education',
+		'fas fa-graduation-cap',
+		'My Education'
+	],
+	[
+		'my_portofolio',
+		'fab fa-squarespace',
+		'My Portofolio'
+	],
+	[
+		'my_blog',
+		'fas fa-file-alt',
+		'My Blog'
+	],
+	[
+		'contact_me',
+		'fa fa-phone',
+		'Contact Me'
+	],
+]
